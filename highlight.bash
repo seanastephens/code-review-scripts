d1ac54f623700b10c89b0245947ea0a18c24a061
Sean Stephens <seanastephens@email.arizona.edu>
2016-02-24 16:24:54 -0700

    Clean up formatting.

--- highlight.bash
+++ highlight.bash
@@ -7,17 +7,18 @@ BROWSER=google-chrome
 
 for file in $@ ; do
 
-	# Right click on a request in chrome dev tools to find "copy as cURL."
+	# Where does this come from? Right click on a request in chrome dev tools 
+	# to find "copy as cURL."
 	curl 'https://tohtml.com/python/' \
 		--data "code_src=$(cat $file)&Submit=Highlight&style=default&type=python" \
-	> ${file}.html
+		> ${file}.html
 
-	HTML_FILES="${HTML_FILES} ${file}.html"
+	html_files="${html_files} ${file}.html"
 
 done
 
-$BROWSER $HTML_FILES
+$BROWSER $html_files
 
 # Clean up, but make sure the browser found them all first.
 sleep 3
-rm $HTML_FILES
+rm $html_files
